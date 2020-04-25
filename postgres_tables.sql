8c6959621ad275d04d673901dd8d79a71cbcd6a1
hannahcjy <59676473+hannahcjy@users.noreply.github.com>
2020-04-25 13:25:48 -0400

    aaa

--- postgres_tables.sql
+++ postgres_tables.sql
@@ -5,3 +5,12 @@ create table unemployment (
 	State text,
 	Rate int
 );
+drop table joblisting;
+create table joblisting (
+	job_title text,
+	location text,
+	organization text,
+	month int,
+	year int,
+	state text
+);
